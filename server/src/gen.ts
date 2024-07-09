1aade34fe1e118fbfa916c8bc2be477f159bf18a
Akshit <akshitgaur2003@gmail.com>
2024-07-09 12:27:41 +0530

    api rate limits

--- server/src/gen.ts
+++ server/src/gen.ts
@@ -17,20 +17,16 @@ const VaporwaveVideoGamePrompt = "ui frame for a tarot-themed video game, window
 
 const SurrealPrompt = "A scene from a polychromatic surrealistic masterpiece, high contrast surreal collage, optical art, neko, crisp, vaporwave, papercut --version 5.2 --ar 16:9, in the scene"
 
-// Middleware to check for NSFW content
 const checkNSFW = async (c: Context, next: Next) => {
   const { prompt } = await c.req.json();
 
   if (nsfwKeywords.some((keyword) => prompt.toLowerCase().includes(keyword))) {
     return c.text("NSFW content is not allowed", 400);
   }
-
-  // Store the prompt in the context's custom data object
   c.set("customPrompt", prompt);
   await next();
 };
 
-// Create a new Hono app for the /gen endpoint
 const genApp = new Hono<{
   Bindings: {
     AI: any;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,27 @@
-import { Hono } from 'hono';
+import { rateLimit, RateLimitBinding, RateLimitKeyFunc } from "@elithrar/workers-hono-rate-limit";
+import { Hono, Context, Next } from "hono";
 import { genApp } from './gen';
 import { cors } from 'hono/cors';
 
-const app = new Hono();
+type Bindings = {
+	RATE_LIMITER: RateLimitBinding;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+const getKey: RateLimitKeyFunc = (c: Context): string => {
+	// Rate limit on each API token by returning it as the key for our
+	// middleware to use.
+	return c.req.header("Authorization") || "";
+};
+
+// Create an instance of our rate limiter, passing it the Rate Limiting bindings
+const rateLimiter = async (c: Context, next: Next) => {
+	return await rateLimit(c.env.RATE_LIMITER, getKey)(c, next);
+};
+
+// Rate limit all routes across our application
+app.use("*", rateLimiter);
 app.use("/*", cors());
 app.route('/gen', genApp);
 
